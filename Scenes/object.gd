62cd7b070b18c2f2e9d8eb7d8d06f8b73b4bf1f8
Loosee64 <lucyarthur1709@gmail.com>
2026-07-22 22:07:21 +0100

    Object inheritance and object interactions

--- Scenes/object.gd
+++ Scenes/object.gd
@@ -1,6 +1,15 @@
 extends Node2D
+@onready var texture_rect: TextureRect = $TextureRect
+
+enum ObjectType {
+	Tool,
+	Interactable
+}
+
+@export var type = ObjectType.Interactable
 
 var dragging = false
+var execute_collision = false
 
 func _process(delta: float) -> void:
 	if dragging:
@@ -10,10 +19,21 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
 		dragging = true
 	elif event.is_action_released("click"):
+		if dragging && type == ObjectType.Tool:
+			visible = false
 		dragging = false
+		if execute_collision && type == ObjectType.Interactable:
+			scale.x = 2
 
 func spawn() -> void:
 	dragging = true
 	position = get_global_mouse_position()
 	print(position)
 	visible = true
+
+func _on_area_entered(area: Area2D) -> void:
+	execute_collision = true
+
+
+func _on_area_exited(area: Area2D) -> void:
+	execute_collision = false
--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-@export var object: StaticBody2D
+@export var object: Area2D
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
